84aa012844efd1f9b97123dad35af56398400b23
wenhaoquantil <wenhao.lin@quantil.com>
2017-05-25 00:37:13 -0700

    Add layout component

---
+++ pages/components/layout/Layout.js
@@ -0,0 +1,69 @@
+import React, {Component} from "react";
+
+
+class Layout extends Component {
+    hasSider(children) {
+        var value = children || [];
+        if (!(value instanceof Array)) {
+            return false;
+        }
+
+       for(let c of children) {
+          if (c.type.displayName === 'Sider') {
+              return true;
+          }
+       }
+       return false;
+    }
+
+
+    render() {
+        var {children} = this.props;
+        var style={};
+        if (this.hasSider(children)) {
+            style["flexDirection"] ="row"
+        } else {
+            style["flexDirection"] ="column"
+        }
+
+
+        return (
+            <div className="layout" style={style}>
+                {children}
+            </div>
+        );
+    }
+}
+
+const Header = ({children})=> (
+    <div className="header">
+        {children}
+    </div>
+);
+
+
+const Content = ({children})=> (
+    <div className="content">
+        {children}
+    </div>
+);
+
+
+
+const Sider = ({children})=> (
+    <div className="sider">
+        {children}
+    </div>
+);
+
+
+const Footer = ({children})=> (
+    <div className="footer">
+        {children}
+    </div>
+);
+
+
+export {Layout, Header, Content, Sider, Footer}
+
+

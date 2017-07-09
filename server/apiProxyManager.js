3a07a9440463741658ff4d1d60c57e72db15179a
wenhaoquantil <wenhao.lin@quantil.com>
2017-07-08 22:44:51 -0700

    don't strip /api

--- server/apiProxyManager.js
+++ server/apiProxyManager.js
@@ -4,13 +4,6 @@ const url = require("url");
 const apiUrl="http://amzanalyticsapi.herokuapp.com:80/";
 
 
-function pathResolver(req) {
-    var path = url.parse(req.url).path;
-    //strip api
-    return path.substr("/api".length);
-}
-
-
 function proxyReqOptDecorator(proxyReqOpts, srcReq){
     proxyReqOpts.headers['Content-Type'] = 'application/json';
     proxyReqOpts.headers['Accept'] = 'application/json';
@@ -21,7 +14,6 @@ function proxyReqOptDecorator(proxyReqOpts, srcReq){
 
 function getApiProxy(){
     return  proxy(apiUrl, {
-        proxyReqPathResolver:  pathResolver,
         proxyReqOptDecorator: proxyReqOptDecorator
     })
 }
